b091e74cfd8ab1233dee859c7205e132cfc51e06
webtoor <fityanalimunshi@gmail.com>
2019-08-03 00:58:56 +0700

    Send email Ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +11,20 @@ class HomeController extends Controller
     }
     public function sendEmail(Request $request){
         $data = $request->all();
-        
+        $user = new \stdClass();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->subject = $data['subject'];  
+        $user->pesan = $data['message'];  
+
+
+        Mail::send('menu.partials.email-send', ['name' => $user->name, 'email' => $user->email, 'subject' => $user->subject, 'pesan' => $user->pesan ], function($mail) use ($user){
+            $mail->from('_mainaccount@fityanali.site','Webtoor');
+            $mail->to('fityanalishi@gmail.com', 'webtoor')->subject($user->subject);
+        });
+
         return response()->json([
             "success" => "true",
-            "data" => $data['name']
         ]);
     }
 }
---
+++ resources/views/menu/partials/email-send.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="app" itemscope itemtype="http://schema.org/Article">
+<head>  
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+<body>
+    <span style="font-family:'comic sans ms','sans-serif'">
+	<p>Name : {{ $name }}  </p>
+	<p>Email : {{ $email }}  </p>
+	<p>Subject : {{ $subject }} </p>
+	<p>Message :   {{ $pesan }}</p>
+    </span>
+</body>
+</html>
